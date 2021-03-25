c63d050e33078b30c20e0b78f0c2a00931f9660e
topcon-pconlon <76080362+topcon-pconlon@users.noreply.github.com>
2021-03-25 11:23:57 +1000

    Tidy bat file

--- components/tokens/get_token.bat
+++ components/tokens/get_token.bat
@@ -5,9 +5,9 @@ set env="qa"
 set dc="us"
 
 rem # Authentication
-set oauth_id="2dc51e38-8b90-479d-a45a-6be25284e465"
-set oauth_secret="d481ab84-056c-454d-9615-f871a5f8558a"
-set oauth_scope="c805593d6a103162fb5df196877ee7df"
+set oauth_id=""
+set oauth_secret=""
+set oauth_scope=""
 
 python get_token.py ^
     --dc %dc% ^
