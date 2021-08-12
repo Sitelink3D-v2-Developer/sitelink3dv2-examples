8fbc21e637ef81cbe10b0f3b9a3650d1d5f5a997
topcon-pconlon <pconlon@TOPCON.COM>
2021-08-12 15:00:45 +1000

    Introduce new workflow example that demonstrates the downloading of haul report trail information and conversion into GPX format for visualization.

---
+++ workflows/create_and_download_haul_report_gpx_trails/create_and_download_haul_report_gpx_trails.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+## Shell file to run a haul report, download the resulting trail data and convert each haul to gpx format for display in third party tools.
+
+## Settings for the environment.
+env="qa"
+dc="us"
+site_id=""
+
+## Authorization. OAuth credentials are used if the JWT string is empty.
+# run `SitelinkFrontend.core.store.getState().app.owner.jwt[0]` in your browser developer console to obtain a JWT.
+jwt=""
+# - or -
+oauth_id=""
+oauth_secret=""
+oauth_scope=""
+
+## Settings for the reports:
+report_iso_date_time_start="2020-12-31 09:21:00"
+report_iso_date_time_end="2021-11-30 17:21:00"
+
+exec python create_and_download_haul_report_gpx_trails.py \
+    --env "$env" \
+    --dc "$dc" \
+    --site_id "$site_id" \
+    --jwt "$jwt" \
+    --oauth_id "$oauth_id" \
+    --oauth_secret "$oauth_secret" \
+    --oauth_scope "$oauth_scope" \
+    --report_iso_date_time_start "$report_iso_date_time_start" \
+    --report_iso_date_time_end "$report_iso_date_time_end"
