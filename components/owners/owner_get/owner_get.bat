549d1f46edf0999d36fd776695a739e0df76d6a6
yeleva <public.yeleva@use.startmail.com>
2022-03-17 11:16:41 +1000

    Introduce new component example that extracts information from the SiteOwner microservice about a specific organization / owner.

---
+++ components/owners/owner_get/owner_get.bat
@@ -0,0 +1,27 @@
+@echo off
+rem ## Batch file to list the specified organization / owner details.
+
+rem ## Settings for the environment.
+set env="qa"
+set dc="us"
+
+rem ## Settings specific to this script.
+rem # The owner identifier is obtained using the process described at https://github.com/Sitelink3D-v2-Developer/sitelink3dv2-examples#owner-identifier
+set owner_id=""
+
+rem ## Authorization
+rem # run `SitelinkFrontend.core.store.getState().app.owner.jwt[0]` in your browser developer console to obtain a JWT.
+set jwt=""
+rem # - or -
+set oauth_id=""
+set oauth_secret=""
+set oauth_scope=""
+
+python owner_get.py ^
+    --env %env% ^
+    --dc %dc% ^
+    --owner_id %owner_id% ^
+    --jwt %jwt% ^
+    --oauth_id %oauth_id% ^
+    --oauth_secret %oauth_secret% ^
+    --oauth_scope %oauth_scope% 
