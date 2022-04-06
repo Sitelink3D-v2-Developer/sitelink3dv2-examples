3d9fee3137fc9e157c56a77abd469bf8b28b02ab
yeleva <public.yeleva@use.startmail.com>
2022-04-06 12:04:33 +1000

    Update comments around the use of RDM to query the details of an individual folder.

--- components/files/folder_create/folder_create.py
+++ components/files/folder_create/folder_create.py
@@ -50,14 +50,40 @@ def make_folder(a_folder_bean, a_server_config, a_site_id, a_headers):
         logging.info("Folder created.")
     logging.debug ("The new folder uuid = {0}".format(a_folder_bean._id))
 
+    # Inserting into RDM is asynchronous so we need to allow for a delay before checking.
+    # In production code, you should subscribe to the events service and respond appropriately before proceeding.
+    #
+    # We will now query RDM for details of the folder we just created to confirm that Sitelink3D v2 now knows about it.
+    # The following code initialises the two parameters we will need to query RDM for information on the folder.
+    # These are "start" and "limit".
+    # 
+    # The start parameter is a base64 encoded key list. In this example we specify a list with a single uuid as the start of the RDM search.
+    # The limit parameter is set to one which produces only one result – confirming the existence of the folder just created only. 
+    #
+    # Produces the list containing the id of the folder that the example previously created as a string
+    id_list_string = json.dumps([a_folder_bean._id])
+    # eg: ["5ae193d3-f1a2-4330-83b5-fab3fb66b7fc"]
 
+    # We send the key list on the wire base64 encoded and to perform that encoding, we need to convert the string into a byte array using
+    id_list_bin = id_list_string.encode('utf-8') 
+    # eg: b'["5ae193d3-f1a2-4330-83b5-fab3fb66b7fc"]'
 
-    # # Inserting into RDM is asyncronous. So we need to allow for a delay before checking.
-    # # In production code, you should subscribe to the events service and respond appropriately.
-    # time.sleep(1)
+    # which we can then pass to the encoder
+    id_list_b64_bin = base64.urlsafe_b64encode(id_list_bin)
+    # eg: b'WyI1YWUxOTNkMy1mMWEyLTQzMzAtODNiNS1mYWIzZmI2NmI3ZmMiXQ=='
 
+    # The encoded byte array can be converted back to a string
+    id_list_b64_string = id_list_b64_bin.decode('utf-8')
+    # eg: WyI1YWUxOTNkMy1mMWEyLTQzMzAtODNiNS1mYWIzZmI2NmI3ZmMiXQ==
+
+    # We are then able to strip the byte padding from the base64 encoded string with 
+    id_list_b64_string_stripped = id_list_b64_string.rstrip("=")
+    # eg: WyI1YWUxOTNkMy1mMWEyLTQzMzAtODNiNS1mYWIzZmI2NmI3ZmMiXQ
+
+    # which then forms part of the complete request
+    limit, start = 1, id_list_b64_string_stripped
     rdm_filesystem_url = "{0}/rdm/v1/site/{1}/domain/file_system/view/_head".format(a_server_config.to_url(), a_site_id)
-    limit, start = 1, base64.urlsafe_b64encode(json.dumps([a_folder_bean._id]).encode('utf-8')).decode('utf-8').rstrip("=")
+    
     response = session.get(rdm_filesystem_url, headers=a_headers, params={"limit":limit,"start":start})
     response.raise_for_status()
     items = response.json()["items"]
