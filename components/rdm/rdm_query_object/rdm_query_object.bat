840bfd2d35168f7a68e72655ae63e5f22874e5b5
paulgconlon <public.yeleva@use.startmail.com>
2022-07-20 07:29:55 +1000

    Introduce new component example demonstrating how a single RDM object in a view can be queried at a site.

---
+++ components/rdm/rdm_query_object/rdm_query_object.bat
@@ -0,0 +1,37 @@
+@echo off
+rem # Batch file to find a single RDM object represented by the specified UUID in a view at a site.
+
+rem ## Settings for the environment.
+set env="qa"
+set dc="us"
+set site_id=""
+
+rem # Use the components/metadata/rdm_list example to discover the domains and associated views at your site.
+set rdm_view="v_sl_task_by_name"
+set rdm_domain="sitelink"
+set rdm_object_uuid=""
+
+set page_limit="100"
+set start=""
+
+rem ## Authorization. OAuth credentials are used if the JWT string is empty.
+rem # run `SitelinkFrontend.core.store.getState().app.owner.jwt[0]` in your browser developer console to obtain a JWT.
+set jwt=""
+rem # - or -
+set oauth_id=""
+set oauth_secret=""
+set oauth_scope=""
+
+python rdm_query_object.py ^
+    --env %env% ^
+    --dc %dc% ^
+    --site_id %site_id% ^
+    --start %start% ^
+    --page_limit %page_limit% ^
+    --rdm_view %rdm_view% ^
+    --rdm_domain %rdm_domain% ^
+    --rdm_object_uuid %rdm_object_uuid% ^
+    --jwt %jwt% ^
+    --oauth_id %oauth_id% ^
+    --oauth_secret %oauth_secret% ^
+    --oauth_scope %oauth_scope%
---
+++ components/rdm/rdm_query_object/rdm_query_object.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import os
+import sys
+from textwrap import indent
+
+def path_up_to_last(a_last, a_inclusive=True, a_path=os.path.dirname(os.path.realpath(__file__)), a_sep=os.path.sep):
+    return a_path[:a_path.rindex(a_sep + a_last + a_sep) + (len(a_sep)+len(a_last) if a_inclusive else 0)]
+
+components_dir = path_up_to_last("components")
+
+sys.path.append(os.path.join(components_dir, "utils"))
+from imports import *
+
+for imp in ["args","get_token", "rdm_traits", "rdm_list", "rdm_pagination_traits"]:
+    exec(import_cmd(components_dir, imp))
+
+session = requests.Session()
+
+def main():
+    # >> Arguments
+
+    arg_parser = argparse.ArgumentParser(description="Paginating RDM data")
+
+    # script parameters:
+    arg_parser = add_arguments_logging(arg_parser, logging.INFO)
+
+    # server parameters:
+    arg_parser = add_arguments_environment(arg_parser)
+    arg_parser = add_arguments_auth(arg_parser)
+
+    arg_parser = add_arguments_pagination(arg_parser)
+
+    # request parameters:
+    arg_parser.add_argument("--site_id", default="", help="Site Identifier", required=True)
+    arg_parser.add_argument("--rdm_view", default="v_sl_task_by_name", help="The view to query RDM by")
+    arg_parser.add_argument("--rdm_domain", default="sitelink", help="The domain that the view exists in")
+    arg_parser.add_argument("--rdm_object_uuid", required=True)
+
+    arg_parser.set_defaults()
+    args = arg_parser.parse_args()
+    logging.basicConfig(format=args.log_format, level=args.log_level)
+    # << Arguments
+
+    server = ServerConfig(a_environment=args.env, a_data_center=args.dc)
+
+    logging.info("Running {0} for server={1} dc={2} site={3}".format(os.path.basename(os.path.realpath(__file__)), server.to_url(), args.dc, args.site_id))
+
+    headers = headers_from_jwt_or_oauth(a_jwt=args.jwt, a_client_id=args.oauth_id, a_client_secret=args.oauth_secret, a_scope=args.oauth_scope, a_server_config=server)
+
+    logging.info("Querying view {}".format(args.rdm_view))
+
+    page_traits = RdmPaginationTraits(a_page_size=args.page_limit, a_start=args.start)
+    more_data = True
+    while more_data:
+        rdm_list = query_rdm_by_domain_view(a_server_config=server, a_site_id=args.site_id, a_domain=args.rdm_domain, a_view=args.rdm_view, a_headers=headers, a_params=page_traits.params())
+        more_data = page_traits.more_data(rdm_list)
+        for fi in rdm_list["items"]:
+            if fi["id"] == args.rdm_object_uuid:
+                print(json.dumps(fi,indent=4))
+                break 
+
+if __name__ == "__main__":
+    main()    
