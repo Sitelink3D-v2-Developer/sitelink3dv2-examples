bfa2493df225f3f72e25c91f6b27875d0eee7d2e
Christophe Baron <cbaron@topcon.com>
2022-08-12 08:57:47 +0100

    Update the on-board-weighing workflow example to include the material in each of the lift events for applications doing mix of materials.

--- workflows/data_live/mfk/stream_weight_events/stream_weight_events.py
+++ workflows/data_live/mfk/stream_weight_events/stream_weight_events.py
@@ -56,7 +56,6 @@ while True:
     msg_json = json.loads(ws.recv())
 
     if msg_json['type'] == "sitelink::Event":
-
         # For all messages, we cache the asset context if the payload specifies one.
         # This allows us to interrogate machine names when processing event payloads.
         ac_uuid = ""
@@ -91,8 +90,26 @@ while True:
                 jobs[job_uuid]["at"] = at
                 jobs[job_uuid]["lifts"] = []
 
-            jobs[job_uuid]["lifts"].append(msg_json["data"])
-            logging.debug("{}, machine '{}' lifted {} tonnes for job {}".format(at, machine_name, msg_json["data"]["weight"], job_uuid))
+            # add the material to the lift event
+            lift_json = msg_json["data"]
+            lift_material_name = ""
+            try:
+                lift_material_uuid = states[ac_uuid]["material"]
+                if len(lift_material_uuid) > 0:
+                    lift_json["material_uuid"] = lift_material_uuid
+                page_traits = RdmPaginationTraits(a_page_size="500", a_start="")
+                rdm_list = query_rdm_by_domain_view(a_server_config=server_https, a_site_id=args.site_id, a_domain="sitelink", a_view="v_sl_material_by_name", a_headers=headers, a_params=page_traits.params())
+                for material in rdm_list["items"]:
+                    if material["id"] == lift_material_uuid:
+                        lift_material_name = material["value"]["name"]
+                        lift_json["material_name"] = lift_material_name
+                        break
+            except KeyError:
+                pass
+
+            # add the lift to the job data
+            jobs[job_uuid]["lifts"].append(lift_json)
+            logging.debug("{}, machine '{}' lifted {} tonnes for job {} {}".format(at, machine_name, '%.2f' % (lift_json["weight"]), job_uuid, "of {}".format(lift_material_name) if len(lift_material_name) > 0 else ""))
 
         # The clear event indicates that the weighing system has finished loading the truck.
         # We can hence aggregate the lift payloads and produce a final weight for the job.
@@ -107,42 +124,56 @@ while True:
                 jobs[job_uuid]["at"] = at
                 jobs[job_uuid]["lifts"] = []
 
-            # aggregate the lifts for this job now that it's closed
+            # aggregate the lifts by materials for this job now that it's closed
             total_weight = 0
-            try:
-                for i, lift in enumerate(jobs[job_uuid]["lifts"]):
-                    total_weight += lift["weight"]
-            except KeyError:
-                pass
+            job_totals = {}
+            job_totals["total"] = 0
+            job_totals["count"] = 0
+            job_totals["materials"] = {}
+            for lift in jobs[job_uuid]["lifts"]:
+                if "material_uuid" not in lift:
+                    job_totals["total"] += lift["weight"]
+                    if lift["weight"] > 0:
+                        job_totals["count"] += 1
+                    else:
+                        job_totals["count"] -= 1
+                else:
+                    lift_material_uuid = lift["material_uuid"]
+                    if lift_material_uuid not in job_totals["materials"]:
+                        job_totals["materials"][lift_material_uuid] = {}
+                        job_totals["materials"][lift_material_uuid]["total"] = 0
+                        job_totals["materials"][lift_material_uuid]["count"] = 0
+                        job_totals["materials"][lift_material_uuid]["material"] = lift["material_name"]
+                    lift_weight = job_totals["materials"][lift_material_uuid]
+                    lift_weight["total"] += lift["weight"]
+                    if lift["weight"] > 0:
+                        lift_weight["count"] += 1
+                    else:
+                        lift_weight["count"] -= 1
+                total_weight += lift["weight"]
 
             output_string = "{}, machine '{}' cleared weighing job '{}' ({})".format(at, machine_name, job_uuid, msg_json["data"]["job_num"])
 
-            material_name = ""
-            try:
-                material_uuid = states[ac_uuid]["material"]
-                page_traits = RdmPaginationTraits(a_page_size="500", a_start="")
-
-                rdm_list = query_rdm_by_domain_view(a_server_config=server_https, a_site_id=args.site_id, a_domain="sitelink", a_view="v_sl_material_by_name", a_headers=headers, a_params=page_traits.params())
-                for material in rdm_list["items"]:
-                    if material["id"] == material_uuid:
-                        material_name = material["value"]["name"]
-                        break
-            except KeyError:
-                pass
-
+            # output the weight data
             if total_weight > 0:
                 lift_count = len(jobs[job_uuid]["lifts"])
-                total_weight = '%.3f'%(total_weight)
-                output_string += " totalling {} tonnes {}".format(total_weight, "of {}".format(material_name) if len(material_name) > 0 else "")
-                output_string += " over {} {}".format(lift_count, "lifts" if lift_count > 1 else "lift")
-                output_string += " (trade legal)" if msg_json["data"]["legal_trade"] else " (not trade legal)"
+                output_string += " totalling {} tonnes".format('%.2f' % (total_weight))
+                output_string += " (legal for trade)" if msg_json["data"]["legal_trade"] else " (not legal for trade)"
+                # output the lift weight data
+                if job_totals["count"] > 0:
+                    lift_count = job_totals["count"]
+                    output_string += " ({} tonnes over {} {} of undefined material)".format('%.2f' % (job_totals["total"]), lift_count, "lifts" if lift_count > 1 else "lift")
+                for material in job_totals["materials"]:
+                    lift_material = job_totals["materials"][material]
+                    lift_count = lift_material["count"]
+                    output_string += " ({} tonnes over {} {} of {})".format('%.2f' % (lift_material["total"]), lift_count, "lifts" if lift_count > 1 else "lift", lift_material["material"])
 
+            # output the truck data
             truck_name = ""
             target_weight = ""
             try:
                 truck_uuid = states[ac_uuid]["truck"]
                 page_traits = RdmPaginationTraits(a_page_size="500", a_start="")
-
                 rdm_list = query_rdm_by_domain_view(a_server_config=server_https, a_site_id=args.site_id, a_domain="sitelink", a_view="v_sl_truck_by_name", a_headers=headers, a_params=page_traits.params())
                 for truck in rdm_list["items"]:
                     if truck["id"] == truck_uuid:
